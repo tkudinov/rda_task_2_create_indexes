db6c737b77e9ebd0267bab13d113db3767240d87
Taras Kudinov <38283923+tkudinov@users.noreply.github.com>
2025-02-10 10:45:29 +0200

    Added indexes for columns in created database.
    Realized solution in additional task.sql file.

--- task.sql
+++ task.sql
@@ -1,2 +1,5 @@
-# Write your SQL code for the database creation here. Good luck! 
-USE ShopDB; 
+USE ShopDB;
+
+CREATE INDEX idx_email ON Customers(EMail);
+CREATE INDEX idx_name ON Products(Name);
+CREATE INDEX idx_date ON Orders(Date);
